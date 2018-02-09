6418f10fe15abfab40333b91fcaeb09fc04c814b
Juan Candelaria Claborne <juan@dreamvent.com>
2018-02-09 10:36:06 -0800

    Bug Fixes (#7)
    * handles changes to the default grade center layout
    
    * handles changes to the default grade center layout
    
    * fixed undefined user issue and changed download naming scheme
    
    * changed download naming scheme by adding remove extention function
    
    * fix undefined user
    
    * fix user bugs
    
    fix undefined user, fix missing first user score from parscore

--- Parscore.js
+++ Parscore.js
@@ -12,7 +12,7 @@ function handleFileSelect(evt) {
             text: "Please only drag and drop CSV files from Blackboard or TXT files from ParScore.",
             icon: "warning",
             buttons: true,
-            dangerMode: true,
+            dangerMode: true
           })
           return;
     }
@@ -21,30 +21,40 @@ function handleFileSelect(evt) {
     reader.onload= function(){
         var text = reader.result;
         if(file.type == "text/csv" || file.type == "application/vnd.ms-excel") {
-            readFileCSV(text);
+            readFileCSV(text, file.name);
         }
         else if(file.type == "text/plain"){
+			console.log("HERE");
             readFileTXT(text,file.name);
         }
     };
     reader.readAsText(file);
 }
-/* Reads in CSV file from Blackboard and parses through to write to text file. */
-function readFileCSV(text) {
+/* Reads in CSV file from Blackboard and parses through to write to text file.
+ * Handles changes to the default grade center scheme. */
+function readFileCSV(text, fileName) {
     var write = "";
+    var uname = fname = lname = 0;
     var allTextLines = text.split(/\r\n|\n/); //split by new line
-    for(var i = 1; i<allTextLines.length;i++){ //skip header line
-        var line = allTextLines[i].split(',');
-        write = write + line[2] + "," + line[0] + "," + line[1] + "\n";
+    var line = allTextLines[0].split(',');
+    //set correct positions for username, first name, and last name
+    for(var i = 0; i < allTextLines[0].length; i++){
+        if(line[i] == "\"Username\"") uname = i;
+        else if(line[i] == "\"First Name\"") fname = i;
+        else if(line[i] == "\"Last Name\"") lname = i;
+    }
+    for(var i = 1; i<allTextLines.length-1;i++){ //skip header line
+        line = allTextLines[i].split(',');
+        write = write + line[uname] + "," + line[lname] + "," + line[fname] + "\n";
         write = write.replace(/['"]+/g, ''); //get rid of string quotes
     }
-    makeTextFile(write);
+    makeTextFile(write, fileName);
 }
 /* Reads in Text file from Parscore and populates an array with the content of the text for CSV format.*/
 function readFileTXT(text, fileName){
     var rows = [["Username",fileName]];
     var allNewlines = text.split(/\r\n|\n/); //split by new line
-    for(var i = 1; i<allNewlines.length-1;i++){ //skip header line
+    for(var i = 0; i<allNewlines.length-1;i++){ //skip header line
         var write = allNewlines[i].split('\t');
         write[0] = write[0].replace(/['"]+/g, ''); //get rid of string quotes
         write[1] = write[1].replace(/['"]+/g, ''); //get rid of string quotes
@@ -57,25 +67,34 @@ function readFileTXT(text, fileName){
         csvContent += row + "\r\n";
     });
     var encodedUri = encodeURI(csvContent);
-    makeCSVFile(encodedUri);
+console.log("HERE");
+    makeCSVFile(encodedUri, fileName);
 }
 /* Creates a text file through a Blob JS object and attaches it to downloads element in CSS.*/
-function makeTextFile(text) {
+function makeTextFile(text, fileName) {
+    fileName = removeExtention(fileName);
     var data = new Blob([text], {type: 'text/plain'});
     var textFile = window.URL.createObjectURL(data);
     var link = document.createElement("a");
     link.setAttribute("href", textFile);
-    link.setAttribute("download", "ToParscore.txt");
+    console.log(fileName)
+    link.setAttribute("download", fileName + ".txt");
     document.body.appendChild(link);
     link.click();
 }
 /* Creates a CSV file and links it with CSS download element. Sets to download in browser.*/
-function makeCSVFile(encodedUri) {
+function makeCSVFile(encodedUri, fileName) {
+    fileName = removeExtention(fileName);
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "ToBlackboard.csv");
+    link.setAttribute("download", fileName+".csv");
     document.body.appendChild(link);
+	console.log("HERE");
     link.click();
+}
+/* Removes the filename extension for filenames passed in*/
+function removeExtention(fileName){
+    return fileName.substr(0,(fileName.length-4));
 }
 /* Prevents default drag behavior for file drop.*/
 function handleDragOver(evt) {
